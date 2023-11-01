17709ab63115d1920680fef3abddc55b71aec162
usman <usmanganishaik99@gmail.com>
2023-11-01 17:41:18 +0530

    Commit

--- 02-EC2/server.tf
+++ 02-EC2/server.tf
@@ -1,8 +1,6 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
-
-
   tags = {
     Name = "HelloWorld"
   }
