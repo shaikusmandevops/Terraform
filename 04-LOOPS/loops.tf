1dba74770e79cc28a7f21b5defb1c6d77beeaeb6
usman <usmanganishaik99@gmail.com>
2023-11-03 11:11:32 +0530

    Commit

--- 04-LOOPS/loops.tf
+++ 04-LOOPS/loops.tf
@@ -4,16 +4,16 @@ variable "components" {
 
 resource "aws_instance" "web" {
   count = length(var.components)
-  ami           = "ami-03265a0778a880afb"
+  ami           = "ami-05a5f6298acdb05b6"
   instance_type = "t3.micro"
-  vpc_security_group_ids =["sg-xxxxxxxxxx"]
+  vpc_security_group_ids =["sg-092c13de3c11aa37f"]
 
   tags = {
     Name = element(var.components, count.index)
   }
-  resource "aws_security_group" "allow_tls" {
-    count=length(var.components)
+ /* resource "aws_security_group" "allow_tls" {
+    //count=length(var.components)
     name   = element(var.components, count.index )
 
-  }
+  }*/
 }
